bb052a4a761f39dd387429819c439318b76f3b12
Nick <nickwoodward5@gmail.com>
2019-01-18 05:52:27 -0800

    Have seeds working through sequelize

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,91 +1,92 @@
--- USE `travel_mateDB`;
--- {
---     "name": "Brad Stevens",
---     "email": "bstevens@hotmail.com",
---     "password": ,
---     "location": "Paris",
---     "interests": "Sports, Outdoors, Night Scene",
---     "about": "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.",
---     "activities": "Blah blah blah",
---     "profilePhoto": "https://images.pexels.com/photos/45882/man-crazy-funny-dude-45882.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
---     "coverPhoto": "https://images.unsplash.com/photo-1495401246624-593eb4b920ba?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d8257fb59fbb7007ed2c200404795641&auto=format&fit=crop&w=800&q=60",
---     "username": "bradS"
--- }
+USE `travel_mateDB`;
 
--- {
---     "name": "Simon Robben",
---     "email": "srobben@gmail.com",
---     "password": ,
---     "location": "Austin",
---     "interests": "Fine Dining, Art & Museums, Music Events, Sight Seeing",
---     "about": "Cupcake ipsum dolor sit. Amet chocolate cake tiramisu jelly beans dragée halvah tiramisu I love. Tootsie roll bear claw fruitcake chocolate I love jujubes toffee fruitcake powder. Marshmallow dessert fruitcake danish biscuit ice cream. Tiramisu lemon drops sesame snaps. Cookie cupcake muffin. Macaroon halvah oat cake caramels danish. Carrot cake halvah chocolate. Caramels danish sesame snaps sweet.",
---     "activities": "It's a blah blah kind of blah blah blah blah blah",
---     "profilePhoto": "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
---     "coverPhoto": "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
---     "username": "sRobben"
--- }
+{
+    "name": "Brad Stevens",
+    "email": "bstevens@hotmail.com",
+    "password": ,
+    "location": "Paris",
+    "interests": "Sports, Outdoors, Night Scene",
+    "about": "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.",
+    "activities": "Blah blah blah",
+    "profilePhoto": "https://images.pexels.com/photos/45882/man-crazy-funny-dude-45882.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "coverPhoto": "https://images.unsplash.com/photo-1495401246624-593eb4b920ba?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d8257fb59fbb7007ed2c200404795641&auto=format&fit=crop&w=800&q=60",
+    "username": "bradS"
+}
 
--- {
---     "name": "Billy Bob",
---     "email": "billbob@gmail.com",
---     "password": "4444",
---     "location": "Paris",
---     "interests": "Fine Dining, Outdoors",
---     "about": "My name is Billy Bob. I am know Paris very well and will show you all the most delicious foods to eat.",
---     "activities": "hiking, paddle boating, dining",
---     "profilePhoto": "https://media.gq.com/photos/56d75ae39acdcf20275f0f4e/master/pass/1-courtesy%20of%20Gucci.jpg",
---     "coverPhoto": "https://www.musictick.com/img/slide/slide_parigi.jpg",
---     "username": "BillyBob"
--- }
+{
+    "name": "Simon Robben",
+    "email": "srobben@gmail.com",
+    "password": ,
+    "location": "Austin",
+    "interests": "Fine Dining, Art & Museums, Music Events, Sight Seeing",
+    "about": "Cupcake ipsum dolor sit. Amet chocolate cake tiramisu jelly beans dragée halvah tiramisu I love. Tootsie roll bear claw fruitcake chocolate I love jujubes toffee fruitcake powder. Marshmallow dessert fruitcake danish biscuit ice cream. Tiramisu lemon drops sesame snaps. Cookie cupcake muffin. Macaroon halvah oat cake caramels danish. Carrot cake halvah chocolate. Caramels danish sesame snaps sweet.",
+    "activities": "It's a blah blah kind of blah blah blah blah blah",
+    "profilePhoto": "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "coverPhoto": "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
+    "username": "sRobben"
+}
 
--- {
---     "name": "Barbara",
---     "email": "babs241@yahoo.com",
---     "password": "4444",
---     "location": "San Francisco",
---     "interests": "Night Scene,  Art & Museums",
---     "about": "Hello! My name is Barbara, I will  travelling to San Francisco next month and would love to see all the amazing museums, art and clubs in the city",
---     "activities": "dancing, museums and art",
---     "profilePhoto": "http://djbrecord.free.fr/barbara019.jpg",
---     "coverPhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSnlmU5YssZUd8aAgKIG5Yl8aBsBqdv7fyJc0K5cdzKpWTLYGq",
---     "username": "babs"
--- }
+{
+    "name": "Billy Bob",
+    "email": "billbob@gmail.com",
+    "password": "4444",
+    "location": "Paris",
+    "interests": "Fine Dining, Outdoors",
+    "about": "My name is Billy Bob. I am know Paris very well and will show you all the most delicious foods to eat.",
+    "activities": "hiking, paddle boating, dining",
+    "profilePhoto": "https://media.gq.com/photos/56d75ae39acdcf20275f0f4e/master/pass/1-courtesy%20of%20Gucci.jpg",
+    "coverPhoto": "https://www.musictick.com/img/slide/slide_parigi.jpg",
+    "username": "BillyBob"
+}
 
--- {
---     "name": "Deb Rah",
---     "email": "debdeb@outlook.com",
---     "password": "4444",
---     "location": "Buenos Aires",
---     "interests": "Outdoors, Camping",
---     "about": "My name is Deb and I live in Buenos Aires. I love everything outdoors and adventure and can show you the beautiful landscapes near by my home town.",
---     "activities": "hiking, biking, exploring",
---     "profilePhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHAmgX2ahZ3zYUo9iP4iTfgz2Lrn2B8Jk6V1SXmFljRoZarYUUQA",
---     "coverPhoto": "https://www.zicasso.com/sites/default/files/styles/original_scaled_down/public/photos/review/1583777/braziliguassufalls-gapargentinaadventure.jpg",
---     "username": "debdeb"
--- }
+{
+    "name": "Barbara",
+    "email": "babs241@yahoo.com",
+    "password": "4444",
+    "location": "San Francisco",
+    "interests": "Night Scene,  Art & Museums",
+    "about": "Hello! My name is Barbara, I will  travelling to San Francisco next month and would love to see all the amazing museums, art and clubs in the city",
+    "activities": "dancing, museums and art",
+    "profilePhoto": "http://djbrecord.free.fr/barbara019.jpg",
+    "coverPhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSnlmU5YssZUd8aAgKIG5Yl8aBsBqdv7fyJc0K5cdzKpWTLYGq",
+    "username": "babs"
+}
 
--- {
---     "name": "Frank",
---     "email": "fbomb@aol.com",
---     "password": "4444",
---     "location": "Sydney",
---     "interests": ,
---     "about": "I want to see new things and everything Sydney has to offer",
---     "activities": "I am friendly, chill and enjoy having a good time",
---     "profilePhoto": "http://www.sinatra.com/sites/default/files/styles/news-thumbnail/public/Frank%20Sinatra%202.25.15%20site%20post.jpg?itok=bH18he2V",
---     "coverPhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyIQ860p-8HW9rkHdCpDr3Qm4vh5IDii4cM4n4wgMW47s9v2bYeQ",
---     "username": "frankie"
--- }
+{
+    "name": "Deb Rah",
+    "email": "debdeb@outlook.com",
+    "password": "4444",
+    "location": "Buenos Aires",
+    "interests": "Outdoors, Camping",
+    "about": "My name is Deb and I live in Buenos Aires. I love everything outdoors and adventure and can show you the beautiful landscapes near by my home town.",
+    "activities": "hiking, biking, exploring",
+    "profilePhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHAmgX2ahZ3zYUo9iP4iTfgz2Lrn2B8Jk6V1SXmFljRoZarYUUQA",
+    "coverPhoto": "https://www.zicasso.com/sites/default/files/styles/original_scaled_down/public/photos/review/1583777/braziliguassufalls-gapargentinaadventure.jpg",
+    "username": "debdeb"
+}
 
--- {
---     "name": "Mary Poppins",
---     "email": "mpoppins@gmail.com",
---     "password": "4444",
---     "location": "London",
---     "interests": "tea, spoons full of sugar",
---     "about": "Hello I am Mary Poppins and live in London, England. I can show you all of the sights of my town. I grew up here and know all of its history.",
---     "activities": "tea parties and flying",
---     "profilePhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2SaJVA3LVxB6BdAtystS1rJgWlhH-u-8dswqtTVlfsA7tpRPq",
---     "coverPhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToWPG_bGU5Bkhk7HmA-biwzMP0jHKtAWvPY5FV7U7KXWg8FNNgDQ",
---     "username": "MaryPoppins"
--- }
+{
+    "name": "Frank",
+    "email": "fbomb@aol.com",
+    "password": "4444",
+    "location": "Sydney",
+    "interests": ,
+    "about": "I want to see new things and everything Sydney has to offer",
+    "activities": "I am friendly, chill and enjoy having a good time",
+    "profilePhoto": "http://www.sinatra.com/sites/default/files/styles/news-thumbnail/public/Frank%20Sinatra%202.25.15%20site%20post.jpg?itok=bH18he2V",
+    "coverPhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyIQ860p-8HW9rkHdCpDr3Qm4vh5IDii4cM4n4wgMW47s9v2bYeQ",
+    "username": "frankie"
+}
+
+{
+    "name": "Mary Poppins",
+    "email": "mpoppins@gmail.com",
+    "password": "4444",
+    "location": "London",
+    "interests": "tea, spoons full of sugar",
+    "about": "Hello I am Mary Poppins and live in London, England. I can show you all of the sights of my town. I grew up here and know all of its history.",
+    "activities": "tea parties and flying",
+    "profilePhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2SaJVA3LVxB6BdAtystS1rJgWlhH-u-8dswqtTVlfsA7tpRPq",
+    "coverPhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToWPG_bGU5Bkhk7HmA-biwzMP0jHKtAWvPY5FV7U7KXWg8FNNgDQ",
+    "username": "MaryPoppins"
+}
---
+++ seeders/20190118133434-travelmate-seeder.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert('Users', [{
+          name: "Brad Stevens",
+          email: "bstevens@hotmail.com",
+          password: 'password',
+          location: "Paris",
+          interests: "Sports, Outdoors, Night Scene",
+          about: "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.",
+          activities: "Blah blah blah",
+          profilePhoto: "https://images.pexels.com/photos/45882/man-crazy-funny-dude-45882.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          coverPhoto: "https://images.unsplash.com/photo-1495401246624-593eb4b920ba?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d8257fb59fbb7007ed2c200404795641&auto=format&fit=crop&w=800&q=60",
+          username: "bradS",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },{
+          name: "Simon Robben",
+          email: "srobben@gmail.com",
+          password: "password",
+          location: "Austin",
+          interests: "Fine Dining, Art & Museums, Music Events, Sight Seeing",
+          about: "Cupcake ipsum dolor sit. Amet chocolate cake tiramisu jelly beans dragée halvah tiramisu I love. Tootsie roll bear claw fruitcake chocolate I love jujubes toffee fruitcake powder. Marshmallow dessert fruitcake danish biscuit ice cream. Tiramisu lemon drops sesame snaps. Cookie cupcake muffin. Macaroon halvah oat cake caramels danish. Carrot cake halvah chocolate. Caramels danish sesame snaps sweet.",
+          activities: "It's a blah blah kind of blah blah blah blah blah",
+          profilePhoto: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          coverPhoto: "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
+          username: "sRobben",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
