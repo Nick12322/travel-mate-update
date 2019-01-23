4a8f4afd3acadddbdc6bcad05931c6286fcf0e2e
Nick <nickwoodward5@gmail.com>
2019-01-23 00:50:11 -0800

    fixed db seeding issue; adding seeds, fixing card display issue

--- seeders/20190118133434-travelmate-seeder.js
+++ seeders/20190118133434-travelmate-seeder.js
@@ -49,7 +49,111 @@ module.exports = {
           interests: "Whatever you want",
           about: "Excited to meet you!!",
           activities: "It's a blah blah kind of blah blah blah blah blah",
-          profilePhoto: "https://steemitimages.com/DQmSA6J7q5C5bMFiHQ85XgBXsYE3fZmBcAmjPvJNYkStBRQ/3403978938_249c74d920_b.jpg",
+          profilePhoto: "https://pbs.twimg.com/profile_images/441506305912889344/o6X0I4il.jpeg",
+          coverPhoto: "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
+          username: "tJohnson",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },{
+          name: "Sara Thompson",
+          email: "sthompson@gmail.com",
+          password: "password",
+          location: "San Diego",
+          interests: "Cool things",
+          about: "Just a girl living my life.",
+          activities: "It's a blah blah kind of blah blah blah blah blah",
+          profilePhoto: "https://assets.capitalfm.com/2018/23/lilliya-scarlett-instagram-1528814125-custom-0.png",
+          coverPhoto: "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
+          username: "tJohnson",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },{
+          name: "Mike Connoly",
+          email: "mconnoly@gmail.com",
+          password: "password",
+          location: "San Diego",
+          interests: "Having a blas",
+          about: "Can't wait to show you around!",
+          activities: "It's a blah blah kind of blah blah blah blah blah",
+          profilePhoto: "https://media.glamour.com/photos/58336fd44e6d66172e161a3a/master/w_1280,c_limit/0117-GL-LIFE01-02.jpg",
+          coverPhoto: "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
+          username: "tJohnson",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },{
+          name: "Mike Ronson",
+          email: "mronson@gmail.com",
+          password: "password",
+          location: "San Diego",
+          interests: "Having a blasttt",
+          about: "Can't wait to show you around!",
+          activities: "It's a blah blah kind of blah blah blah blah blah",
+          profilePhoto: "https://images.pexels.com/photos/447189/pexels-photo-447189.jpeg?cs=srgb&dl=adult-business-businessman-447189.jpg&fm=jpg",
+          coverPhoto: "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
+          username: "tJohnson",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },{
+          name: "Melanie Ronson",
+          email: "melronson@gmail.com",
+          password: "password",
+          location: "San Diego",
+          interests: "Having a blasttt",
+          about: "I love adventures!",
+          activities: "It's a blah blah kind of blah blah blah blah blah",
+          profilePhoto: "https://timedotcom.files.wordpress.com/2018/01/stephanie-schriock-motto.jpg",
+          coverPhoto: "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
+          username: "tJohnson",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },{
+          name: "Vera Lamb",
+          email: "vlamb@gmail.com",
+          password: "password",
+          location: "Atlanta",
+          interests: "Having a blasttt",
+          about: "I love adventures!",
+          activities: "It's a blah blah kind of blah blah blah blah blah",
+          profilePhoto: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+          coverPhoto: "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
+          username: "tJohnson",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },{
+          name: "Ryan Tomas",
+          email: "rtomas@gmail.com",
+          password: "password",
+          location: "Atlanta",
+          interests: "Having a blasttt",
+          about: "I love adventures!",
+          activities: "It's a blah blah kind of blah blah blah blah blah",
+          profilePhoto: "http://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/470/2015/10/23122001/CommonMan.jpg",
+          coverPhoto: "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
+          username: "tJohnson",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },{
+          name: "Liza Gonzalez",
+          email: "lgonzalez@gmail.com",
+          password: "password",
+          location: "San Francisco",
+          interests: "Having a blasttt",
+          about: "I love adventures!",
+          activities: "It's a blah blah kind of blah blah blah blah blah",
+          profilePhoto: "https://www.newonline.org/sites/default/files/images/blog/Diversity_MuslimWomen_Krawczyk.jpg",
+          coverPhoto: "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
+          username: "tJohnson",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },{
+          name: "Archie Stevens",
+          email: "astevens@gmail.com",
+          password: "password",
+          location: "San Francisco",
+          interests: "Having a blasttt",
+          about: "I love adventures!",
+          activities: "It's a blah blah kind of blah blah blah blah blah",
+          profilePhoto: "https://nationalpostcom.files.wordpress.com/2018/08/cpt501-the-canadian-press3.jpg?quality=80&strip=all&w=780",
           coverPhoto: "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
           username: "tJohnson",
           createdAt: new Date(),
@@ -59,12 +163,8 @@ module.exports = {
     },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+   
+      return queryInterface.bulkDelete('Users', null, {});
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
   }
 };
